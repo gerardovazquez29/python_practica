d5d290490d161e4ce6fe99ef4351ed2954c70e36
Gerardo Santiago Vazquez Tovar <neowhatup@gmail.com>
2025-09-24 18:09:41 -0600

    miercoles24

--- ejercicios/ejercicio_prac.py
+++ ejercicios/ejercicio_prac.py
@@ -1,13 +1,38 @@
 # Ejercicios propuestos (sin solución)
 
 # 1-Crea una función que reciba dos números y devuelva cuál es mayor.
-
-
+def num_mayor(num1,num2):
+    if num1 > num2 :
+        mayor = num1
+    elif num2 > num1:
+        mayor = num2
+    else:
+        mayor = "son iguales"
+    return print(f"el numero mayor es:{mayor}")
+num_mayor(5,7) # el numero mayor es:7
+            
 # 2-Crea una función que calcule el área de un círculo (usa π ≈ 3.1416).
+def area_circulo(radio):
+    return 3.1416 * radio **2
+radio = 6
+area = area_circulo(radio)
+print(f"El area del circulo con radio {radio} es: {area:.2f}")
+  #  El area del circulo con radio 6 es: 113.10
 
 
 # 3-Escribe una función que reciba un número y diga si es primo o no.
-
+def num_primo(num):
+    """ if num > 2 :
+        return False
+    if num == 2:
+        return True
+    if num % 2 == 0:
+        return False """
+    for i in range(3, int(num**0.5) + 1,2):
+        if num % i == 0:
+            return False
+    return True
+print(num_primo(7))
 
 # 4-Crea una función que reciba una lista de números y devuelva la suma de los pares.
 
