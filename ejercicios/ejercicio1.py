c20320837e94ee4e951d4e6a0e6bbac25bd5cedf
Gerardo Santiago Vazquez Tovar <neowhatup@gmail.com>
2025-09-25 18:16:29 -0600

    ju25

---
+++ ejercicios/ejercicio1.py
@@ -0,0 +1,62 @@
+# Funciones + Condicionales
+
+# -Escribe una función es_par(n) que reciba un número y 
+#  devuelva True si es par y False si es impar.
+
+ 
+# -Escribe una función clasificar_edad(edad) que regrese 
+#  "Niño", "Joven", "Adulto", "Mayor" según la edad.
+
+
+# Funciones + Bucles
+ 
+# -Crea una función suma_lista(lista) que reciba una lista de números 
+#  y devuelva la suma de todos ellos.
+
+
+# -Escribe una función contar_vocales(texto) que reciba un string y 
+#  devuelva cuántas vocales tiene.
+
+
+# Funciones + Listas
+ 
+# -Crea una función invertir_lista(lista) que regrese una 
+#  lista al revés sin usar .reverse().
+
+
+# -Define una función duplicar_numeros(lista) que devuelva 
+#  una nueva lista con cada número multiplicado por 2.
+
+
+# Funciones + Tuplas
+ 
+# -Crea una función max_min(tupla) que reciba una tupla 
+#  de números y regrese el valor máximo y mínimo.
+
+
+# -Escribe una función buscar_elemento(tupla, valor) que 
+#  indique si el elemento está dentro de la tupla.
+
+
+# Funciones + Diccionarios
+
+
+# -Define una función contar_palabras(texto) que regrese 
+#  un diccionario con las palabras como claves y el número 
+#  de repeticiones como valores.
+
+
+# -Crea una función agregar_contacto(diccionario, nombre, telefono) 
+#  que agregue un contacto a un diccionario de teléfonos.
+
+
+# Funciones + Sets
+
+
+# -Crea una función union_sets(set1, set2) que devuelva la unión 
+#  de dos conjuntos.
+
+
+# -Haz una función elementos_unicos(lista) que tome una lista y 
+#  devuelva los elementos únicos usando un set.
+
